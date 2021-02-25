4fb6ff11f5de0e6238a765f4e7905ab3fd83b34c
Dominik Fleischmann <dominik.fleischmann@canonical.com>
2021-02-25 15:49:39 +0100

    Change Minio Interface Library

--- operators/argo-controller/lib/charms/minio/v0/minio_interface.py
+++ operators/argo-controller/lib/charms/minio/v0/minio_interface.py
@@ -1,5 +1,8 @@
 """
 Minio Interface
+
+This Interface requires the serialized-data-interface package
+pleas add it to your requirements.txt
 """
 
 # The unique Charmhub library identifier, never change it
@@ -10,7 +13,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft push-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 1
+LIBPATCH = 2
 
 from provide_interface import ProvideAppInterface
 from require_interface import RequireAppInterface
@@ -19,27 +22,19 @@ from ops.charm import CharmBase
 
 # Serialized Data Schema for Minio Interface
 MINIO_SCHEMA="""
-type: object
-properties:
-  service:
-    type: string
-  port:
-    type: number
-  access-key:
-    type: string
-  secret-key:
-    type: string
+service:
+  type: string
+port:
+  type: number
+access-key:
+  type: string
+secret-key:
+  type: string
 """
 
 class MinioProvide(ProvideAppInterface):
-    def __init__(
-        self, charm: CharmBase, relation_name: str
-    ):
-        super().__init__(charm, relation_name, MINIO_SCHEMA)
+    SCHEMA = MINIO_SCHEMA
 
 
 class MinioRequire(RequireAppInterface):
-    def __init__(
-        self, charm: CharmBase, relation_name: str
-    ):
-        super().__init__(charm, relation_name, MINIO_SCHEMA)
+    SCHEMA = MINIO_SCHEMA
