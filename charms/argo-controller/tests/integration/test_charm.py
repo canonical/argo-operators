7ee8675262182ff49ff5ee292f1de0eabac01770
Andrew Scribner <ca.scribner+1@gmail.com>
2022-07-22 10:17:31 -0400

    feat: bump grafana and prometheus to latest/edge in integration tests

--- charms/argo-controller/tests/integration/test_charm.py
+++ charms/argo-controller/tests/integration/test_charm.py
@@ -137,8 +137,8 @@ async def test_prometheus_grafana_integration(ops_test: OpsTest):
     prometheus_scrape_charm = "prometheus-scrape-config-k8s"
     scrape_config = {"scrape_interval": "30s"}
 
-    await ops_test.model.deploy(prometheus, channel="latest/beta", trust=True)
-    await ops_test.model.deploy(grafana, channel="latest/beta", trust=True)
+    await ops_test.model.deploy(prometheus, channel="latest/edge", trust=True)
+    await ops_test.model.deploy(grafana, channel="latest/edge", trust=True)
     await ops_test.model.add_relation(
         f"{prometheus}:grafana-dashboard", f"{grafana}:grafana-dashboard"
     )
