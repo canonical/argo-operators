cd6890cc22381fb09ec321af3c8360cff3ddbe43
i-chvets <113444075+i-chvets@users.noreply.github.com>
2023-09-02 12:10:39 -0400

    feat: get images on main (#124)
    * feat: get images on main
    https://github.com/canonical/bundle-kubeflow/issues/679
    
    Summary of changes:
    - Added script that produces list of container images managed by charm in this repository. Image list is a dynamic list.
    - Removed outdated get-images-1.7-stable.sh script.

--- tools/get-images.sh
+++ tools/get-images.sh
@@ -2,15 +2,9 @@
 #
 # This script returns list of container images that are managed by this charm and/or its workload
 #
-# static list
-STATIC_IMAGE_LIST=(
-)
-# dynamic list
-git checkout origin/track/3.3
 IMAGE_LIST=()
 IMAGE_LIST+=($(find -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
 VERSION=$(grep upstream-source charms/argo-controller/metadata.yaml | awk -F':' '{print $3}')
-IMAGE_LIST+=($(grep "executor_image =" charms/argo-controller/src/charm.py | awk '{print $3}' | sed s/f//g | sed s/\"//g | sed s/{version}/$VERSION/g))
-
-printf "%s\n" "${STATIC_IMAGE_LIST[@]}"
+IMAGE_LIST+=($(yq '.options.executor-image.default' ./charms/argo-controller/config.yaml))
 printf "%s\n" "${IMAGE_LIST[@]}"
+
