ce2f72111744fce6fb81b91a0fcf83c363978660
Daniela Plascencia <daniela.plascencia@canonical.com>
2024-04-15 10:47:55 +0200

    skip: fix channel

--- charms/argo-controller/tests/integration/test_charm.py
+++ charms/argo-controller/tests/integration/test_charm.py
@@ -16,20 +16,20 @@ CHARM_ROOT = "."
 ARGO_CONTROLLER = "argo-controller"
 ARGO_CONTROLLER_TRUST = True
 MINIO = "minio"
-MINIO_CHANNEL = "ckf-1.8/stable"
+MINIO_CHANNEL = "latest/edge"
 MINIO_CONFIG = {
     "access-key": "minio",
     "secret-key": "minio-secret-key",
 }
 
 PROMETHEUS_K8S = "prometheus-k8s"
-PROMETHEUS_K8S_CHANNEL = "1.0/stable"
+PROMETHEUS_K8S_CHANNEL = "latest/stable"
 PROMETHEUS_K8S_TRUST = True
 GRAFANA_K8S = "grafana-k8s"
-GRAFANA_K8S_CHANNEL = "1.0/stable"
+GRAFANA_K8S_CHANNEL = "latest/stable"
 GRAFANA_K8S_TRUST = True
 PROMETHEUS_SCRAPE_K8S = "prometheus-scrape-config-k8s"
-PROMETHEUS_SCRAPE_K8S_CHANNEL = "1.0/stable"
+PROMETHEUS_SCRAPE_K8S_CHANNEL = "latest/stable"
 PROMETHEUS_SCRAPE_CONFIG = {"scrape_interval": "30s"}
 log = logging.getLogger(__name__)
 
