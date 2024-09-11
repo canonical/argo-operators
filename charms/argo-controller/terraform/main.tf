fcc84703131f813adf946e811af5408987a5f061
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2024-09-11 16:10:44 +0300

    reorder main.tf

--- charms/argo-controller/terraform/main.tf
+++ charms/argo-controller/terraform/main.tf
@@ -1,13 +1,13 @@
 resource "juju_application" "argo_controller" {
-  name      = var.app_name
-  model     = var.model_name
-  trust     = true
-  resources = var.resources
   charm {
     name     = "argo-controller"
     channel  = var.channel
     revision = var.revision
   }
-  units  = 1
   config = var.config
+  model     = var.model_name
+  name      = var.app_name
+  resources = var.resources
+  trust     = true
+  units  = 1
 }
