7583879a32dc703d7e695e5f8fc5ac4fa439aa18
mvlassis <manos.vlassis@canonical.com>
2025-07-24 13:46:28 +0200

    Add explanatory comment

--- charms/argo-controller/tests/unit/test_charm.py
+++ charms/argo-controller/tests/unit/test_charm.py
@@ -147,7 +147,8 @@ def test_kubernetes_created_method(
     harness.charm.on.install.emit()
 
     # FIXME: This is a hardcoded count of the Kubernetes objects that should be created.
-    # The `reconcile` function is called twice, so we expect 2 apply calls for each resource
+    # The `reconcile` function is called twice, once for `object_storage_relation_changed`
+    # and once for `install`, so we expect 2 apply calls for each resource
     assert mocked_lightkube_client.apply.call_count == 28
     assert isinstance(harness.charm.kubernetes_resources.status, ActiveStatus)
 
