ab236e654f252778dedc6aa49e13542b75825009
Noha Ihab <49988746+NohaIhab@users.noreply.github.com>
2025-08-04 13:26:55 +0300

    chore: update terraform module channel to 3.5/stable (#284)

--- charms/argo-controller/terraform/variables.tf
+++ charms/argo-controller/terraform/variables.tf
@@ -7,7 +7,7 @@ variable "app_name" {
 variable "channel" {
   description = "Charm channel"
   type        = string
-  default     = "latest/edge"
+  default     = "3.5/stable"
 }
 
 variable "config" {
