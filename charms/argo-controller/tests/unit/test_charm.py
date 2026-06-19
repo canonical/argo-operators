808dfc34a8a7cb00437f311fc6a1af914ff5e8e8
mvlassis <manos.vlassis@canonical.com>
2026-06-19 11:47:37 +0300

    Update unit tests

--- charms/argo-controller/tests/unit/test_charm.py
+++ charms/argo-controller/tests/unit/test_charm.py
@@ -4,7 +4,6 @@
 from unittest.mock import MagicMock, PropertyMock, patch
 
 import pytest
-from charmed_kubeflow_chisme.components import S3RequirerComponent
 from charmed_kubeflow_chisme.testing import add_sdi_relation_to_harness
 from ops.model import ActiveStatus, BlockedStatus
 from ops.testing import Harness
